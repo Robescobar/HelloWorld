a5d4b2c061e7596cf8c3847493a9e89d4de4b7fa
Sós Róbert <sosrobert20011031@gmail.com>
2023-03-06 17:34:01 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("Ez már sárga színű!");
+            Console.ReadKey();
+        }
+    }
+}
